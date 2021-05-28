085494099b1dbdee8d4f49e3ca4ba0fe7f5b273a
Vladimir <39743965+vokzvokz@users.noreply.github.com>
2021-05-28 12:26:56 +0300

    fix

--- Heuristic_Clustering/__init__.py
+++ Heuristic_Clustering/__init__.py
@@ -1,6 +1,6 @@
 
 from .HeuristicClusteringExecutor import run
 from .utils import pandas_to_spark, debugging_printer
-from .custom_algorithms import CustomPowerIterationClustering
+# from .custom_algorithms import CustomPowerIterationClustering
 
-__all__ = ['run', 'pandas_to_spark', 'CustomPowerIterationClustering']
+__all__ = ['run', 'pandas_to_spark']
--- Heuristic_Clustering/custom_algorithms/CustomPowerIterationClustering.py
+++ Heuristic_Clustering/custom_algorithms/CustomPowerIterationClustering.py
@@ -1,4 +1,4 @@
-from pyspark.sql.functions import udf,col
+from pyspark.sql.functions import udf, col
 from pyspark.sql.types import *
 from pyspark.ml.feature import Interaction, VectorAssembler
 from pyspark.sql.functions import desc, row_number, monotonically_increasing_id
@@ -6,7 +6,8 @@ from pyspark.sql.window import Window
 from scipy.spatial import distance
 from pyspark.ml.clustering import PowerIterationClustering
 
-class CustomPIC:
+
+class CustomPowerIterationClustering:
     def __init__(self, predictionCol='labels', **configuration):
         self.algorithm = PowerIterationClustering(**configuration)
         self.predictionCol = predictionCol
@@ -14,11 +15,11 @@ class CustomPIC:
 
     def fit_transform(self, data):
         udffunc = udf(self.dist_func, FloatType())
-        res = data.select('features')\
-                  .withColumn('src', row_number().over(Window.orderBy(monotonically_increasing_id())))
-        res = res.join(res.toDF('features2', 'dst'))\
-                 .filter("src < dst")\
-                 .withColumn("weight",udffunc("features", "features2"))\
-                 .drop('features', 'features2')
+        res = data.select('features') \
+            .withColumn('src', row_number().over(Window.orderBy(monotonically_increasing_id())))
+        res = res.join(res.toDF('features2', 'dst')) \
+            .filter("src < dst") \
+            .withColumn("weight", udffunc("features", "features2")) \
+            .drop('features', 'features2')
         clustered_df = self.algorithm.assignClusters(res).withColumnRenamed('cluster', self.predictionCol)
         return clustered_df
